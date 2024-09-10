9ea1bb1815d9a4ad45667148fb6b7e76f2e755df
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-10 11:27:57 +0300

    fix: add correct code to flake.nix and default.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
-  pkgs ? import <nixpkgs> { },
-}:
-let
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-in
-pkgs.rustPlatform.buildRustPackage {
-  pname = manifest.name;
-  version = manifest.version;
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
+  description = "Generate Podman Quadlet files from a Podman command, compose file, or existing object ";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./. { };
+      });
+    };
 }
